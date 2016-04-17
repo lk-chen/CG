667ab71eb8f2c8562fdb18d8087b4a3f485d279d
lkchen-x1c <knight0clk@hotmail.com>
2016-04-17 04:42:34 -0400

    feat:Complete UI

--- cgdemo/Intersection.Designer.cs
+++ cgdemo/Intersection.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.btnDraw = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblCoord = new System.Windows.Forms.Label();
+            this.ckbAnimation = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // btnDraw
@@ -41,22 +45,71 @@
             this.btnDraw.UseVisualStyleBackColor = true;
             this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
             // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(250, 40);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(131, 73);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load...";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(469, 40);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(141, 96);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // lblCoord
+            // 
+            this.lblCoord.AutoSize = true;
+            this.lblCoord.Location = new System.Drawing.Point(80, 567);
+            this.lblCoord.Name = "lblCoord";
+            this.lblCoord.Size = new System.Drawing.Size(70, 25);
+            this.lblCoord.TabIndex = 3;
+            this.lblCoord.Text = "Coord";
+            // 
+            // ckbAnimation
+            // 
+            this.ckbAnimation.AutoSize = true;
+            this.ckbAnimation.Location = new System.Drawing.Point(852, 40);
+            this.ckbAnimation.Name = "ckbAnimation";
+            this.ckbAnimation.Size = new System.Drawing.Size(137, 29);
+            this.ckbAnimation.TabIndex = 4;
+            this.ckbAnimation.Text = "animation";
+            this.ckbAnimation.UseVisualStyleBackColor = true;
+            // 
             // Intersection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1013, 640);
+            this.ClientSize = new System.Drawing.Size(1340, 697);
+            this.Controls.Add(this.ckbAnimation);
+            this.Controls.Add(this.lblCoord);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.btnDraw);
+            this.MinimumSize = new System.Drawing.Size(1200, 675);
             this.Name = "Intersection";
             this.Text = "Intersection";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Intersection_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Intersection_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Intersection_MouseUp);
+            this.Resize += new System.EventHandler(this.Intersection_Resize);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblCoord;
+        private System.Windows.Forms.CheckBox ckbAnimation;
     }
 }
--- cgdemo/Intersection.cs
+++ cgdemo/Intersection.cs
@@ -13,10 +13,103 @@ namespace cgdemo
     public partial class Intersection : Form
     {
         private List<PointF> segmentEndPoints = new List<PointF>();
+        private int pointDiameter = 4;
+        private Color pointColor = Color.Red;
+        private Color segmentColor = Color.Black;
+        private Color tagColor = Color.Purple;
+        private BufferedGraphics[] bufferLayers;
+        private const int margin = 12;
+        private const int btnWidth = 80;
+        private const int btnHeight = 40;
+        private enum BufferLayerType
+        {
+            TBackground,
+            TSegments,
+            TIntersections,
+            TAnimation,
+        };
 
         public Intersection()
         {
             InitializeComponent();
+            setLayout();
+            initBufferLayers();
+        }
+
+        /// <summary>
+        /// Set static layout, then call dynamic layout set.
+        /// </summary>
+        private void setLayout()
+        {
+            foreach (Control b in this.Controls)
+                if (b is Button)
+                {
+                    b.Width = btnWidth;
+                    b.Height = btnHeight;
+                }
+            btnDraw.Left = margin;
+            btnDraw.Top = margin;
+
+            btnLoad.Left = btnDraw.Right + margin;
+            btnLoad.Top = margin;
+            btnSave.Left = btnLoad.Right + margin;
+            btnSave.Top = margin;
+
+            setDynamicLayout();
+        }
+
+        /// <summary>
+        /// Set dynamic layout
+        /// </summary>
+        private void setDynamicLayout()
+        {
+            lblCoord.Left = margin;
+            lblCoord.Top = ClientSize.Height - lblCoord.Height - margin;
+
+            ckbAnimation.Top = margin;
+            ckbAnimation.Left = ClientSize.Width - ckbAnimation.Width - margin;
+        }
+
+        /// <summary>
+        /// Initialize each buffer layer.
+        /// </summary>
+        private void initBufferLayers()
+        {
+            bufferLayers = new BufferedGraphics[Enum.GetNames(typeof(BufferLayerType)).Length];
+
+            var curContext = BufferedGraphicsManager.Current;
+            for (int i = 0; i < bufferLayers.Length; i++)
+                bufferLayers[i] = curContext.Allocate(
+                    CreateGraphics(), DisplayRectangle);
+            bufferLayers[(int)BufferLayerType.TBackground].Graphics.Clear(BackColor);
+
+            reDrawSegments();
+        }
+
+        /// <summary>
+        /// Draw segments on buffer.
+        /// </summary>
+        private void reDrawSegments()
+        {
+            bufferLayers[(int)BufferLayerType.TBackground].Render(
+                bufferLayers[(int)BufferLayerType.TSegments].Graphics);
+
+            using (var pen = new Pen(segmentColor))
+                using(var tagBrush = new SolidBrush(tagColor))
+            {
+                for (int i = 0; i < segmentEndPoints.Count; i += 2)
+                {
+                    var p1 = segmentEndPoints[i];
+                    var p2 = segmentEndPoints[i + 1];
+                    bufferLayers[(int)BufferLayerType.TSegments].Graphics
+                        .DrawLine(pen, p1, p2);
+                    bufferLayers[(int)BufferLayerType.TSegments].Graphics
+                        .DrawString((i / 2).ToString(), SystemFonts.DefaultFont,
+                        tagBrush, new PointF((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2));
+                }
+            }
+
+            drawIntPoint(new PointF[0]);
         }
 
         /// <summary>
@@ -25,28 +118,53 @@ namespace cgdemo
         /// <param name="intPoints"> Array of intersection points </param>
         private void drawIntPoint(PointF[] intPoints)
         {
-            var g = CreateGraphics();
-            var brush = new SolidBrush(Color.Red);
-            var diameter = 8;
-            foreach (var point in intPoints)
+            bufferLayers[(int)BufferLayerType.TSegments].Render
+                (bufferLayers[(int)BufferLayerType.TIntersections].Graphics);
+
+            using(var brush = new SolidBrush(pointColor))
             {
-                g.FillEllipse(brush, point.X - diameter / 2, point.Y - diameter / 2,
-                    diameter, diameter);
+                foreach (var point in intPoints)
+                    bufferLayers[(int)BufferLayerType.TIntersections].Graphics
+                        .FillEllipse(brush, point.X - pointDiameter / 2, point.Y - pointDiameter / 2,
+                    pointDiameter, pointDiameter);
             }
-            brush.Dispose();
-            g.Dispose();
+
+            bufferLayers[(int)BufferLayerType.TIntersections].Render();
         }
 
+        /// <summary>
+        /// Call back function when user does not want animation.
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="eventi"></param>
+        /// <param name="eventj"></param>
+        /// <param name="slopeIdx"></param>
+        /// <param name="SLSIdx"></param>
+        /// <param name="nexti"></param>
+        /// <param name="nextj"></param>
         private void callbackDoNothing(double y, int eventi, int eventj, int slopeIdx, List<UInt32> SLSIdx, int nexti, int nextj) { }
 
-        private void callbackShowMessage(double y, int eventi, int eventj, int slopeIdx, List<UInt32> SLSIdx, int nexti, int nextj) {
+        /// <summary>
+        /// Call back function to show animation.
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="eventi"></param>
+        /// <param name="eventj"></param>
+        /// <param name="slopeIdx"></param>
+        /// <param name="SLSIdx"></param>
+        /// <param name="nexti"></param>
+        /// <param name="nextj"></param>
+        private void callbackShowAnimation(double y, int eventi, int eventj, int slopeIdx, List<UInt32> SLSIdx, int nexti, int nextj) {
             Console.WriteLine("{0},{0}", y, SLSIdx.Count);
         }
 
         private void btnDraw_Click(object sender, EventArgs e)
         {
-            wrapper.Intersection.CSCallbackType cb = callbackDoNothing;
-            var intPointTuples = wrapper.Intersection.getIntersection(segmentEndPoints, cb);
+            List<Tuple<PointF, uint, uint>> intPointTuples;
+            if (!ckbAnimation.Checked)
+                intPointTuples = wrapper.Intersection.getIntersection(segmentEndPoints, callbackDoNothing);
+            else
+                intPointTuples = wrapper.Intersection.getIntersection(segmentEndPoints, callbackShowAnimation);
 
             PointF[] intPoints = new PointF[intPointTuples.Count];
             for (int i = 0; i < intPoints.Length; i++)
@@ -62,13 +180,27 @@ namespace cgdemo
 
         private void Intersection_MouseUp(object sender, MouseEventArgs e)
         {
+            var p1 = segmentEndPoints.Last();
             segmentEndPoints.Add(new PointF(e.X, e.Y));
-            using (var g = CreateGraphics())
-            {
-                g.DrawLine(new Pen(Color.Black),
-                    segmentEndPoints[segmentEndPoints.Count - 2],
-                    segmentEndPoints[segmentEndPoints.Count - 1]);
-            }
+            var p2 = segmentEndPoints.Last();
+
+            bufferLayers[(int)BufferLayerType.TSegments].Graphics
+                .DrawLine(new Pen(segmentColor), p2, p1);
+            bufferLayers[(int)BufferLayerType.TSegments].Graphics
+                .DrawString((segmentEndPoints.Count / 2).ToString(), SystemFonts.DefaultFont,
+                new SolidBrush(tagColor), new PointF((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2));
+
+            drawIntPoint(new PointF[0]);
+        }
+
+        private void Intersection_Resize(object sender, EventArgs e)
+        {
+            setDynamicLayout();
+        }
+
+        private void Intersection_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblCoord.Text = '(' + e.X.ToString() + ',' + e.Y.ToString() + ')';
         }
     }
 }
